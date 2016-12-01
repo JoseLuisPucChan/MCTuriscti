61a93c62bd893fdce67ab2ddc691139bf9e95f35
JoseLuisPucChan <josechan211@gmail.com>
2016-12-01 14:29:11 -0600

    AddDAO

---
+++ Proyecto0.1/MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/Services/CtrlDireccion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSMCTuristic_CentroHistorico.Services
+{
+    public class CtrlDireccion
+    {
+        DAO.DireccionDAO oDireccion;
+
+        public int InsertarDireccion(object obj)
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.AgregarDireccion(obj);
+        }
+
+        public int ModificarDireccion(object obj)
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.ActualizarDireccion(obj);
+        }
+
+        public int EliminarDireccion(object obj)
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.EliminarDireccion(obj);
+        }
+
+        public DataSet topDireccionDS()
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.DireccionDS();
+        }
+
+        public int InsertarDireccion_admin(object obj)
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.AgregarDireccion_admin(obj);
+        }
+
+        public int ModificarDireccion_admin(object obj)
+        {
+            oDireccion = new DAO.DireccionDAO();
+            return oDireccion.ActualizarDireccion_admin(obj);
+        }
+
+
+    }
+}
