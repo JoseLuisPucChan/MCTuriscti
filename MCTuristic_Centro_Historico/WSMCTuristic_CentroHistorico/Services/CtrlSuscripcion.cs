15d1cb0aca261fcdf378220f22a45c8e857dd99a
JoseLuisPucChan <josechan211@gmail.com>
2016-12-01 01:47:17 -0600

    Proyecto

---
+++ MCTuristic_Centro_Historico/WSMCTuristic_CentroHistorico/Services/CtrlSuscripcion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace WSMCTuristic_CentroHistorico.Services
+{
+    public class CtrlSuscripcion
+    {
+        DAO.SuscripcionDAO oSuscripcion;
+        public int InsertarSuscripcion(object obj)
+        {
+            oSuscripcion = new DAO.SuscripcionDAO();
+            return oSuscripcion.AgregarSuscripcion(obj);
+        }
+
+        public int ModificarSuscripcion(object obj)
+        {
+            oSuscripcion = new DAO.SuscripcionDAO();
+            return oSuscripcion.ActualizarSuscripcion(obj);
+        }
+
+        public int EliminarSuscripcion(object obj)
+        {
+            oSuscripcion = new DAO.SuscripcionDAO();
+            return oSuscripcion.EliminarSuscripcion(obj);
+        }
+        public DataSet topSuscripcionDS()
+        {
+            oSuscripcion = new DAO.SuscripcionDAO();
+            return oSuscripcion.suscripcionDS();
+        }
+
+    }
+}
