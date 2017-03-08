5d273d0a0516fd2a679a30e54dc2249655d88d33
JoseLuisPucChan <josechan211@gmail.com>
2017-03-08 00:52:11 -0600

    Parte movil

---
+++ ProyectoMovil/MCTuristicRutasTuriricas/app/src/main/java/com/example/luispuc/mcturisticrutasturiricas/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.example.luispuc.mcturisticrutasturiricas;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng Merida = new LatLng(
+                20.9673702, -89.59258569999997
+        );
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(20.9673702, -89.59258569999997), new LatLng(20.9441486, -89.5639119))
+                .width(5)
+                .color(Color.RED));
+
+        // Add a marker in Sydney and move the camera
+
+        mMap.addMarker(new MarkerOptions().position(Merida).title("Markador en Mérida"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Merida));
+    }
+}
